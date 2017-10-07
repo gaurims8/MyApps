4f27021a7fff9103b22f6c160b4d73ce31e6e561
Gauri <Gauri@Gauri-PC>
2017-10-07 23:48:34 +0530

    sonarLint changes

--- src/prac/CheckingNull.java
+++ src/prac/CheckingNull.java
@@ -3,10 +3,11 @@ package prac;
 public class CheckingNull {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 		Parent p=new Child();
-		//p.print(null);
+		Parent p1=new Child();
+		p.equals(p1);
 	}
 
 }
